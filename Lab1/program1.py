d596a3fd5fa1dbd3ab0a413cdb7c633133fcf455
zishraq <z.ishraq@gmail.com>
2022-02-09 21:16:30 +0600

    lab0 problem1 code upgraded

--- Lab1/program1.py
+++ Lab1/program1.py
@@ -1,7 +1,13 @@
 def product_or_sum(num1: float, num2: float):
     result = num1 * num2
     if result <= 1000:
+        if int(result) == result:
+            return int(result)
+
         return result
+
+    if int(num1 + num2) == num1 + num2:
+        return int(num1 + num2)
 
     return num1 + num2
 
