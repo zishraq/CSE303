7f9715a779b7b099c4fc27762f0c4ba1330e9309
zishraq <z.ishraq@gmail.com>
2022-02-20 22:06:11 +0600

    Lab 1 problem 6 updated

--- Lab1/program06.py
+++ Lab1/program06.py
@@ -1,16 +1,16 @@
 def get_nth_fibonacci(n):
-    num1 = 0
-    num2 = 1
-
     if n == 0 or n == 1:
         return n
 
-    else:
-        for i in range(1, n):
-            num3 = num1 + num2
-            num1 = num2
-            num2 = num3
-        return num2
+    num1 = 0
+    num2 = 1
+
+    for i in range(1, n):
+        num3 = num1 + num2
+        num1 = num2
+        num2 = num3
+
+    return num2
 
 
 N = int(input('Enter N: '))
