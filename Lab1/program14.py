70e4eb315124a6da43a7627caecd589c618d17c2
zishraq <z.ishraq@gmail.com>
2022-02-22 14:09:15 +0600

    Lab 1 solution 14: funcation name renamed

--- Lab1/program14.py
+++ Lab1/program14.py
@@ -1,13 +1,13 @@
 my_string = str(input('Enter any string: '))
 
 
-def isPalindrome(my_str: str):
+def palindrome_checker_2019_2_60_022(my_str: str):
     if my_str == my_str[::-1]:
         return True
     return False
 
 
-if isPalindrome(my_string):
+if palindrome_checker_2019_2_60_022(my_string):
     print('The string is palindrome')
 
 else:
